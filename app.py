9c6d9e68066d037099ed4bf7bf9847830778af52
Daniel Lee <daniel8lee58@gmail.com>
2022-10-30 15:26:33 -0500

    edits

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+# from flask_cors import CORS  # comment this on deployment
+
+app = Flask(__name__, static_url_path='', static_folder='client/build')
+CORS(app)  # comment this on deployment
+api = Api(app)
+
+
+@app.route("/", defaults={'path': ''})
+def serve(path):
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/test')
+def handle_checkOut_hardware():
+    return "this works"
