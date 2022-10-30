ee2a6adee735379e2a3a5e8ab7c19bfc32ae0fed
Daniel Lee <daniel8lee58@gmail.com>
2022-10-30 15:46:19 -0500

    edits

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request
 from flask_restful import Api, Resource, reqparse
-# from flask_cors import CORS  # comment this on deployment
+from flask_cors import CORS  # comment this on deployment
 
 app = Flask(__name__, static_url_path='', static_folder='client/build')
-# CORS(app)  # comment this on deployment
+CORS(app)  # comment this on deployment
 api = Api(app)
 
+
+# @app.route('/')
+# def index():
+#     return app.send_static_file('index.html')
 
 @app.route("/", defaults={'path': ''})
 def serve(path):
