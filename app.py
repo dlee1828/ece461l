7a9614236ac207d6a6d5207f6ac63265ad7c2d8a
Daniel Lee <daniel8lee58@gmail.com>
2022-10-31 11:51:59 -0500

    edits

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, send_from_directory, request
 from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS  # comment this on deployment
+from flask_cors import CORS
+from pymongo import MongoClient  # comment this on deployment
 
 app = Flask(__name__, static_url_path='', static_folder='client/build')
 CORS(app)  # comment this on deployment
 api = Api(app)
 
+password = "qx4BnukGCF7NMvGY"
+connection_string = f"mongodb+srv://dlee1828:{password}@cluster0.ah7ruoj.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(connection_string)
+print(client.list_database_names())
 
 # @app.route('/')
 # def index():
 #     return app.send_static_file('index.html')
+
 
 @app.route("/", defaults={'path': ''})
 def serve(path):
